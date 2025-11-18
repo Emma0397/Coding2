78f5bbdb5e8bc1c2273deb040cad7c87fe0cf35f
Emma Mancini <emma.mancini@studenti.isissgobetti.it>
2025-11-18 12:18:26 +0100

    Primo commit

---
+++ Coding2/Alunno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding2
+{
+    internal class Alunno
+    {
+        public string nome;
+        public string cognome;  
+        public Alunno(string nome, string cognome)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+        }
+    }
+}
---
+++ Coding2/Espressione.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Coding2
+{
+    internal class Espressione
+    {
+        private string esp1 = "(ciao)";
+        private string esp2 = "[ciao]";
+        private string esp3 = "[a-oA-O]";
+
+        private bool Match1(Alunno alunno)
+        {
+            bool ris = false;
+            Match result = Regex.Match(alunno.cognome, esp1);
+            if (result.Success)
+            {
+                ris = true;
+            }
+            return ris;
+        }
+        private bool Match2(Alunno alunno)
+        {
+            bool ris = false;
+            Match result = Regex.Match(alunno.cognome, esp2);
+            if (result.Success)
+            {
+                ris = true;
+            }
+            return ris;
+        }
+        private bool Match3(Alunno alunno)
+        {
+            bool ris = false;
+            Match result = Regex.Match(alunno.cognome, esp3);
+            if (result.Success)
+            {
+                ris = true;
+            }
+            return ris;
+        }
+    }
+}
